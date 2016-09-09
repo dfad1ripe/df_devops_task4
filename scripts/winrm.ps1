090144dab908074e234b534070f3ccb92b66cd8c
Dmytro <dfad1ripe@gmail.com>
2016-09-09 15:39:03 +0300

    Box for 2012 w/o updates can be created successfully

--- scripts/winrm.ps1
+++ scripts/winrm.ps1
@@ -13,25 +13,20 @@ Remove-Item -Path WSMan:\Localhost\listener\listener* -Recurse
 write-output "Setting up WinRM"
 write-host "(host) setting up WinRM"
 
-Read-Host "Press ENTER"
 cmd.exe /c winrm quickconfig -q
 cmd.exe /c winrm quickconfig '-transport:http'
-Read-Host "Press ENTER"
 cmd.exe /c winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
-cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="1024"}'
+cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="2048"}'
 cmd.exe /c winrm set "winrm/config/service" '@{AllowUnencrypted="true"}'
 cmd.exe /c winrm set "winrm/config/client" '@{AllowUnencrypted="true"}'
 cmd.exe /c winrm set "winrm/config/service/auth" '@{Basic="true"}'
 cmd.exe /c winrm set "winrm/config/client/auth" '@{Basic="true"}'
 cmd.exe /c winrm set "winrm/config/service/auth" '@{CredSSP="true"}'
 cmd.exe /c winrm set "winrm/config/listener?Address=*+Transport=HTTP" '@{Port="5985"}'
-Read-Host "Press ENTER"
 cmd.exe /c netsh advfirewall firewall set rule group="remote administration" new enable=yes
 cmd.exe /c netsh advfirewall firewall add rule name="In 5985" protocol=TCP dir=in localport=5985 action=allow
 cmd.exe /c netsh advfirewall firewall add rule name="Out 5985" protocol=TCP dir=out localport=5985 action=allow
-Read-Host "Press ENTER"
 cmd.exe /c net stop winrm
 cmd.exe /c sc config winrm start= auto
 cmd.exe /c net start winrm
 cmd.exe /c wmic useraccount where "name='vagrant'" set PasswordExpires=FALSE
-Read-Host "Press ENTER"
