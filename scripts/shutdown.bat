15227cc8c7f3dcf45a75d740b93c31fef6a522a5
Dmytro <dfad1ripe@gmail.com>
2016-09-11 20:07:43 +0300

    Corrected syntax of shutdown script

--- scripts/shutdown.bat
+++
@@ -1,3 +0,0 @@
-netsh advfirewall firewall set rule name="In 5985" new action=block
-
-C:/windows/system32/sysprep/sysprep.exe /generalize /oobe /unattend:C:/Windows/Panther/Unattend/unattend.xml /quiet /shutdown
